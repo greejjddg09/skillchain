0c859174caab2ce5bf4570331cbc04169c46edde
greejjddg09 <127425061+greejjddg09@users.noreply.github.com>
2025-06-25 17:32:35 +0500

    Add files via upload

---
+++ Home.js
@@ -0,0 +1,165 @@
+// Получаем элементы слайдера
+const slider = document.querySelector('.slider');
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+const slides = Array.from(slider.querySelectorAll('img'));
+const slideCount = slides.length;
+let slideIndex = 0;
+
+// Устанавливаем обработчики событий для кнопок
+prevButton.addEventListener('click', showPreviousSlide);
+nextButton.addEventListener('click', showNextSlide);
+
+// Функция для показа предыдущего слайда
+function showPreviousSlide() {
+  slideIndex = (slideIndex - 1 + slideCount) % slideCount;
+  updateSlider();
+}
+
+// Функция для показа следующего слайда
+function showNextSlide() {
+  slideIndex = (slideIndex + 1) % slideCount;
+  updateSlider();
+}
+
+// Функция для обновления отображения слайдера
+function updateSlider() {
+  slides.forEach((slide, index) => {
+    if (index === slideIndex) {
+      slide.style.display = 'block';
+    } else {
+      slide.style.display = 'none';
+    }
+  });
+}
+
+// Инициализация слайдера
+updateSlider();
+const langButtons = document.querySelectorAll("[data-btn]");
+const allLangs = ["ru", "en", "de"];
+const currentPathName = window.location.pathname;
+let currentLang =
+	localStorage.getItem("language") || checkBrowserLang() || "ru";
+let currentTexts = {};
+
+const homeTexts = {
+	"home_page-title": {
+		ru: "SKILL CHAIN",
+		en: "SKILL CHAIN",
+		uz: "SKILL CHAIN",
+	},
+	"home":{
+		ru:"Главная",
+		en:"Home",
+		uz:"Bosh Sahifa",
+	},
+	"books": {
+		ru: "Книги",
+		en: "Books",
+		uz: "Kitoblar",
+	},
+	"timetable": {
+		ru: "Расписание",
+		en: "Timetable",
+		uz: "Jadval ",
+	},
+	"administration": {
+		ru: "Администрация",
+		en: "Administration",
+		uz: "Administratsiya",
+	},
+	"teachers": {
+		ru: "Педагогический состав",
+		en: "Teachers",
+		uz: "Ustozlar",
+	},
+	"proverb": {
+		ru: "СОСАЛИ?",
+		en: "SOSALi?",
+		uz: "SOSALI?",
+	}
+};
+function checkPagePathName() {
+	switch (currentPathName) {
+		case "/index.html":
+			currentTexts = homeTexts;
+			break;
+
+		default:
+			currentTexts = homeTexts;
+			break;
+	}
+}
+checkPagePathName();
+
+// Изменение языка у текстов
+function changeLang() {
+	for (const key in currentTexts) {
+		let elem = document.querySelector(`[data-lang=${key}]`);
+		if (elem) {
+			elem.textContent = currentTexts[key][currentLang];
+		}
+	}
+}
+changeLang();
+
+// Вешаем обработчики на каждую кнопку
+langButtons.forEach((btn) => {
+	btn.addEventListener("click", (event) => {
+		if (!event.target.classList.contains("header__btn_active")) {
+			currentLang = event.target.dataset.btn;
+			localStorage.setItem("language", event.target.dataset.btn);
+			resetActiveClass(langButtons, "header__btn_active");
+			btn.classList.add("header__btn_active");
+			changeLang();
+		}
+	});
+});
+
+// Сброс активного класса у переданного массива элементов
+function resetActiveClass(arr, activeClass) {
+	arr.forEach((elem) => {
+		elem.classList.remove(activeClass);
+	});
+}
+
+// Проверка активной кнопки
+function checkActiveLangButton() {
+	switch (currentLang) {
+		case "ru":
+			document
+				.querySelector('[data-btn="ru"]')
+				.classList.add("header__btn_active");
+			break;
+		case "en":
+			document
+				.querySelector('[data-btn="en"]')
+				.classList.add("header__btn_active");
+			break;
+		case "de":
+			document
+				.querySelector('[data-btn="de"]')
+				.classList.add("header__btn_active");
+			break;
+
+		default:
+			document
+				.querySelector('[data-btn="ru"]')
+				.classList.add("header__btn_active");
+			break;
+	}
+}
+checkActiveLangButton();
+
+// Проверка языка браузера
+function checkBrowserLang() {
+	const navLang = navigator.language.slice(0, 2).toLowerCase();
+	const result = allLangs.some((elem) => {
+		return elem === navLang;
+	});
+	if (result) {
+		return navLang;
+	}
+}
+
+console.log("navigator.language", checkBrowserLang());
